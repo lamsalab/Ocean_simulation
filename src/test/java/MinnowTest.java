d043dd091ca619980cd8dee3416f7c9f96949fc7
Shelby Frazier <shelbyfrazi@gmail.com>
2017-03-22 13:30:27 -0500

    Fix MinnowTest

--- src/test/java/MinnowTest.java
+++ src/test/java/MinnowTest.java
@@ -8,6 +8,6 @@ import static org.junit.Assert.assertEquals;
 public class MinnowTest {
     @Test
     public void update() throws Exception {
-        assertEquals("update({}, 0)",null, new Fish(5, new Point(0, 0)).update(Collections.emptySet(), IntStream.of(0)));
+        assertEquals("update({}, 0)",null, new Minnow(new Point(0, 0), 5).update(Collections.emptySet(), IntStream.of(0)));
     }
 }
