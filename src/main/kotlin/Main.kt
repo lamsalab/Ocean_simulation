ad8fc23333507f18769cf1b5441f0bc6f232182b
lamsalab <am.lamsal@gmail.com>
2017-03-22 15:01:49 -0500

    Added Main

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+import java.util.*
+
+class Main {
+
+    val width = 10
+    val height = 10
+
+    var allObjects: MutableList<AquariumObject> = Collections.emptyList<AquariumObject>()
+    var ocean: MutableList<Char> = Collections.emptyList()
+
+    fun generateObjects() {
+
+    }
+
+    fun updateObjects() {
+
+    }
+
+    fun updateOcean() {
+
+    }
+
+    fun displayOcean() {
+
+    }
+
+    fun main(args: Array<String>) {
+
+
+    }
+}
