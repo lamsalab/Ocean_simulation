f3cc9590abeb14412517bed334817fb86d2b35d5
Shelby Frazier <shelbyfrazi@gmail.com>
2017-03-23 10:12:59 -0500

    Add some EgTest cases

--- src/main/kotlin/Fish.kt
+++ src/main/kotlin/Fish.kt
@@ -9,7 +9,7 @@ interface Fish : AquariumObject {
     }
 
     fun nextPositionRaw(randomInt: Int): Point {
-        return when (randomInt.mod(4)) {
+        return when (randomInt % 4) {
             0 -> Point(position.x, position.y + 1)
             1 -> Point(position.x - 1, position.y)
             2 -> Point(position.x, position.y - 1)
@@ -17,6 +17,9 @@ interface Fish : AquariumObject {
         }
     }
 
+    @Eg(construct = arrayOf("new Point(1, 1)"),
+        given = arrayOf("new Aquarium(5, 5), 1"),
+        returns = "new Point(0, 1)")
     fun nextPosition(aq: Aquarium, randomInt: Int): Point {
         return aq.wrap(nextPositionRaw(randomInt))
     }
