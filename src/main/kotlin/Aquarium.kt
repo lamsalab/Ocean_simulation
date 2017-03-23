86a7e746a91e5993edbdeb00eacf8299bc32731f
Shelby Frazier <shelbyfrazi@gmail.com>
2017-03-23 11:04:33 -0500

    Add EgTests

--- src/main/kotlin/Aquarium.kt
+++ src/main/kotlin/Aquarium.kt
@@ -5,8 +5,8 @@ class Aquarium(w: Int, h: Int) {
     val height = h
 
     @Eg(construct = arrayOf("5", "10"),
-        given = arrayOf("new Point(5, 5)"),
-        returns = "new Point(0, 5)")
+            given = arrayOf("new Point(5, 5)"),
+            returns = "new Point(0, 5)")
     fun wrap(point: Point): Point {
         return Point(point.x.mod(width),point.y.mod(height))
     }
--- src/main/kotlin/Fish.kt
+++ src/main/kotlin/Fish.kt
@@ -8,6 +8,9 @@ interface Fish : AquariumObject {
         val MAXIMUM_CAPACITY = 5
     }
 
+    @Eg(construct = arrayOf("new Point(1, 1)"),
+            given = arrayOf("new Aquarium(5, 5), 1"),
+            returns = "new Point(0, 1)")
     fun nextPositionRaw(randomInt: Int): Point {
         return when (randomInt % 4) {
             0 -> Point(position.x, position.y + 1)
@@ -18,8 +21,8 @@ interface Fish : AquariumObject {
     }
 
     @Eg(construct = arrayOf("new Point(1, 1)"),
-        given = arrayOf("new Aquarium(5, 5), 1"),
-        returns = "new Point(0, 1)")
+            given = arrayOf("new Aquarium(5, 5), 1"),
+            returns = "new Point(0, 1)")
     fun nextPosition(aq: Aquarium, randomInt: Int): Point {
         return aq.wrap(nextPositionRaw(randomInt))
     }
