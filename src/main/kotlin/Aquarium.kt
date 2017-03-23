75b03d2d0c9829cea4c897867cd33975b322761f
David Leppik <dleppik@vocalabs.com>
2017-03-23 09:58:48 -0500

    Adding EgTest, bumping Kotlin version to 1.1.1.

--- src/main/kotlin/Aquarium.kt
+++ src/main/kotlin/Aquarium.kt
@@ -1,7 +1,12 @@
+import com.vocalabs.egtest.annotation.Eg
+
 class Aquarium(w: Int, h: Int) {
     val width = w
     val height = h
 
+    @Eg(construct = arrayOf("5", "10"),
+        given = arrayOf("new Point(5, 5)"),
+        returns = "new Point(0, 5)")
     fun wrap(point: Point): Point {
         return Point(point.x.mod(width),point.y.mod(height))
     }
