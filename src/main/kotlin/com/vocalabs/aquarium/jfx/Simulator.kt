6ddcd4409926f5fe22057c947ed9b3abcb62df44
Shelby Frazier <shelbyfrazi@gmail.com>
2017-03-24 17:01:32 -0500

    Add different colors

--- src/main/kotlin/com/vocalabs/aquarium/jfx/Simulator.kt
+++ src/main/kotlin/com/vocalabs/aquarium/jfx/Simulator.kt
@@ -27,27 +27,26 @@ class Simulator(val gc: GraphicsContext, var x: Double, var y: Double) : Runnabl
         }
     }
 
-
     private fun drawOcean(gc: GraphicsContext, ocean: List<AquariumObject>, aquarium: Aquarium) {
 
-        gc.setFill(Color.BLUE)
+        gc.setFill(Color.STEELBLUE)
         gc.fillRect(0.0, 0.0, this.x, this.y)
-        for (y in 0..aquarium.height) {
-            for (x in 0..aquarium.width) {
+        for (y in 0..(aquarium.height - 1)) {
+            for (x in 0..(aquarium.width - 1)) {
                 for (obj in ocean) {
                     if (obj.position.equals(Point(x, y))) {
                         when (obj) {
                             is Plant -> {
-                                gc.setFill(Color.GREEN)
+                                gc.setFill(Color.SEAGREEN)
                                 gc.fillOval(obj.position.x * 50.0, obj.position.y * 50.0, 50.0, 50.0)
                             }
                             is Minnow -> {
-                                gc.setFill(Color.YELLOW)
+                                gc.setFill(Color.TAN)
                                 gc.fillOval(obj.position.x * 50.0, obj.position.y * 50.0, 50.0, 50.0)
 
                             }
                             is Shark -> {
-                                gc.setFill(Color.ORANGE)
+                                gc.setFill(Color.SLATEGRAY)
                                 gc.fillOval(obj.position.x * 50.0, obj.position.y * 50.0, 50.0, 50.0)
                             }
                         }
